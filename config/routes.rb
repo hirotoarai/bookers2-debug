956a38ae1c7d917e6ab8d4ef4c34c98c1a92e213
EC2 Default User <ec2-user@ip-172-31-8-23.ec2.internal>
2020-11-22 14:41:37 +0000

    いいねコメント非同期

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy, :show]
     resources :book_comments, only: [:create, :destroy, :show]
   end
+  resources :favorites, only: [:index]
 end
