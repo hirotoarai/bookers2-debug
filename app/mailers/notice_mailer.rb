e93505d02f3986265d9218e8b208aeddb8f63085
EC2 Default User <ec2-user@ip-172-31-8-23.ec2.internal>
2020-11-25 05:23:31 +0000

    mail send

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,8 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.greeting.subject
+  #
+end
---
+++ config/initializers/mail.rb
@@ -0,0 +1,17 @@
+if Rails.env.production?
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
+  port: 587,
+  user_name: 'ah940325@gmail.com',
+  password: 'ah9403250901',
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+
+elsif Rails.env.development?
+  ActionMailer::Base.delivery_method = :letter_opener
+else
+  ActionMailer::Base.delivery_method = :test
+end
